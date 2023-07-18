ee704c312e0883e44bab961ffaaa5857af9d2b26
Chrinovic MM <20mm364@esisalama.org>
2023-07-18 04:58:46 -0400

    cetered image

--- app/src/main/java/com/example/birthday_card/MainActivity.kt
+++ app/src/main/java/com/example/birthday_card/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.happybirth_day
+package com.example.birthday_card
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,26 +6,32 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import com.example.birthday_card.ui.theme.BirthdaycardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HappyBirthdayTheme {
+            BirthdaycardTheme() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    BirthdayGreetingWithText( message = "Happy Birthday Sam!", from = "- from Emma")
+                    BirthdayGreetingWithImage("Happy Birthday Shukrani !", "From: Chrinovic MM")
                 }
             }
         }
@@ -33,23 +39,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+fun BirthdayGreetingWithText(
+    message: String, 
+    from: String,
+    modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         Text(
             text = message,
-            fontSize = 36.sp,
+            fontSize = 20.sp
         )
         Text(
             text = from,
-            fontSize = 24.sp,
+            fontSize = 10.sp
         )
+    }
+
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Box{
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
-    HappyBirthdayTheme {
-        BirthdayGreetingWithText(message = "Happy Birthday Sam!", from ="- from Emma")
+    BirthdaycardTheme() {
+        BirthdayGreetingWithText("Happy Birthday Sam!", "From: chrinovic MM")
     }
 }
